fb1874c49cf6b541a6a5459ec984d1228fd41660
mohon63 <mohonhossen63@gmail.com>
2022-08-04 22:56:17 +0600

    jhj

--- first.js
+++ first.js
@@ -36,16 +36,42 @@
 // var result = subtract(100, 50);
 // console.log('resutl value: ', result)
 // console.log('resutl value: ', result)
+// console.log('resutl value: ', result)
 
 
 
-function getFactorial(num) {
-    let factorial = 1;
-    for (let i = 1; i <= num; i++) {
-        factorial = factorial * i;
+// function getFactorial(num) {
+//     let factorial = 1;
+//     for (let i = 1; i <= num; i++) {
+//         factorial = factorial * i;
+//     }
+//     return factorial;
+// }
+
+// var firstFactorial = getFactorial(7);
+// console.log('My factorial ', firstFactorial);
+
+// function arryTotal(numbers) {
+//     let sum = 0;
+//     for (let i = 0; i < numbers.length; i++) {
+//         const element = numbers[i];
+//         sum = sum + element;
+//     }
+//     return sum;
+// }
+// const total = arryTotal([20, 30, 50]);
+// console.log('Array sum : ', total)
+
+function largestElement(numbers) {
+    let largest = numbers[0];
+    for (let i = 0; i < numbers.length; i++) {
+        const element = numbers[i];
+        if (element < largest) {
+            largest = element;
+        }
     }
-    return factorial;
+    return largest;
 }
-
-var firstFactorial = getFactorial(7);
-console.log('My factorial ', firstFactorial);
+const largestNumber = largestElement([2, 5, 1, 10, 12]);
+const lowestNumber = largestElement([-2, -5, -1, -10, -12]);
+console.log('largest number ', largestNumber);
