7c611114ccc1500137cf988d61c8ae5e93b42dce
mohon63 <mohonhossen63@gmail.com>
2022-08-08 21:58:04 +0600

    hh

--- js/event.js
+++ js/event.js
@@ -1,3 +1,6 @@
 document.getElementById("make-blue").addEventListener('click', function () {
     document.body.style.backgroundColor = 'blue';
 })
+// document.getElementById("make-blue").addEventListener('click', function () {
+//     document.body.style.backgroundColor = 'blue';
+// })
